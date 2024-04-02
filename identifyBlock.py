d8a58bc66f65355af71e21e3b3fe13761db9a2f8
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-04-02 15:47:58 +0200

    Modification to support a range of blocks

--- identifyBlock.py
+++ identifyBlock.py
@@ -6,11 +6,12 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env.
 API_KEY = os.getenv("DUNE_KEY")
 
-def listBundles(block: int) -> list:
+def listBundles(blockMin: int, blockMax: int = 0) -> list:
+    blockMax = max(blockMin, blockMax)+1
     query = 3583631
     url = f"https://api.dune.com/api/v1/query/{query}/execute"  # Replace with the actual API URL
     headers = {"X-DUNE-API-KEY": API_KEY}
-    result = requests.request("POST", url, headers=headers, params = {'blockNumber' : block}).json()
+    result = requests.request("POST", url, headers=headers, params = {'blockMin' : blockMin, 'blockMax' : blockMax}).json()
 
     execution_id = result['execution_id']
     while True:
@@ -23,7 +24,10 @@ def listBundles(block: int) -> list:
     url = f"https://api.dune.com/api/v1/execution/{execution_id}/results"
     headers = {"X-DUNE-API-KEY": API_KEY}
     result = requests.request("GET", url, headers=headers).json()['result']
-    txs = [i['transactions'] for i in result['rows']]
+    txs = [[] for i in range(blockMin, blockMax)]
+    for i in result['rows']:
+        txs[i['blockNumber']-blockMin].append(i['transactions'])
     return txs
 
+listBundles(19567000, 19567334)
 
