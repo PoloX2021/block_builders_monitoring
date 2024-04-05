76ed3fc1f47cd7dad2ab095d69a49286010cc583
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-04-05 16:03:21 +0200

    queries all transactions from mev blocker with a backrun

--- identifyBlock.py
+++ identifyBlock.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()  # take environment variables from .env.
 API_KEY = os.getenv("DUNE_KEY")
+API_KEY = 'JYLDIVjVB4qX40CKDGFqead4ZuvDrEav'
 
-def listBundles(blockMin: int, blockMax: int = 0) -> list:
+def list_bundles(blockMin: int, blockMax: int = 0) -> list:
     blockMax = max(blockMin, blockMax)+1
     query = 3583631
     url = f"https://api.dune.com/api/v1/query/{query}/execute"  # Replace with the actual API URL
@@ -23,7 +24,10 @@ def listBundles(blockMin: int, blockMax: int = 0) -> list:
 
     url = f"https://api.dune.com/api/v1/execution/{execution_id}/results"
     headers = {"X-DUNE-API-KEY": API_KEY}
-    result = requests.request("GET", url, headers=headers).json()['result']
+    result = requests.request("GET", url, headers=headers).json()
+    print(result)
+    result = result['result']
+    return result['rows']
     txs = [[] for i in range(blockMin, blockMax)]
     for i in result['rows']:
         txs[i['blockNumber']-blockMin].append(i['transactions'])
