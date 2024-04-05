11ca78a5b7ac75fd974697dbfacf55509c2ed4b5
pauljanicot@yahoo.com <pauljanicot@yahoo.com>
2024-04-05 16:02:28 +0200

    filters all blocks with a blocknumber and sets them in a separate table

--- filterBlock.py
+++ filterBlock.py
@@ -4,7 +4,7 @@ import re, json
 input_file_path = "c:/Users/Paul CoW/Documents/MEVBlocker/Agnostic_relay/table"
 
 # Define the path of the output file
-output_file_path = "c:/Users/Paul CoW/Documents/MEVBlocker/Agnostic_relay/block19511131.txt"
+output_file_path = "c:/Users/Paul CoW/Documents/MEVBlocker/Agnostic_relay/block19511148.txt"
 
 
 # Open the input file in read mode
@@ -19,10 +19,17 @@ with open(output_file_path, "w") as output_file:
     for line in lines:
         a = re.split(' |\t', line.strip())
         # Check if the line meets the criteria
+        if i<=57:
+            print(a)
         if i>57:
-            if json.loads(a[7])['ExecutionPayload']['blockNumber'] == 19511131:
-                # Write the line to the output file
-                output_file.write(line)
+            try: 
+                b = json.loads(a[7])
+                if int(b['ExecutionPayload']['block_number']) == 19511148:
+                    # Write the line to the output file
+                    output_file.write(line)
+            except:
+                print("Error")
+                print(a)
         i+=1
-    if i%100==0:
-        print(i)
+        if i%100==0:
+            print(i)
